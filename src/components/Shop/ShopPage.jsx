83bf8fa87c2669b16ae9b005b86aae0764e9365f
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-08-18 22:21:41 +0530

    add item json and display items

--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import { products } from "../../data/products";
+
 const ShopPage = () => {
+  const [items, setItems] = useState([]);
+
+  //products.map((product, index) => {
+  //  console.log(product.id + " u calling me twice, dawg?");
+  //});
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = () => {
+    products.forEach((product, index) => {
+      setItems((prev) => [...prev, product]);
+    });
+  };
+
   return (
     <div>
       <h1>this is ShopPage</h1>
+      <div className="flex flex-col  items-center justify-center gap-5">
+        {items.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className=" bg-gray-200 p-4 rounded-lg drop-shadow-lg "
+            >
+              <div>
+                <img src={item.image} alt={item.id} className="w-52" />
+              </div>
+              <div>{item.name}</div>
+              <div>{item.price}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/data/products.js
@@ -0,0 +1,85 @@
+import Gpu3060ti from "../images/3060ti.png";
+import Gpu3060tiPreview from "../images/3060ti-2.jpg";
+import Gpu3070ti from "../images/3070ti.png";
+import Gpu3070tiPreview from "../images/3070ti-2.jpg";
+import Gpu3080 from "../images/3080.png";
+import Gpu3080Preview from "../images/3080-2.jpg";
+import Gpu3090 from "../images/3090.png";
+import Gpu3090Preview from "../images/3090-2.jpg";
+import duckyMini from "../images/duckyMini-1.png";
+import duckyMiniPreview from "../images/duckyMini-2.jpeg";
+import keychronK2 from "../images/keychron-k2-1.jpg";
+import keychronK2Preview from "../images/keychron-k2-2.jpg";
+import keychronK6 from "../images/Keychron-K6-1.jpg";
+import keychronK6Preview from "../images/Keychron-K6-2.jpg";
+import keychronK8 from "../images/Keychron-K8-1.jpg";
+import keychronK8Preview from "../images/Keychron-K8-2.jpg";
+
+const products = [
+  {
+    id: 1,
+    name: "3060ti Graphics Card",
+    price: "100$",
+    category: "gpu",
+    image: Gpu3060ti,
+    preview: Gpu3060tiPreview,
+  },
+  {
+    id: 2,
+    name: "3070ti Graphics Card",
+    price: "100$",
+    category: "gpu",
+    image: Gpu3070ti,
+    preview: Gpu3070tiPreview,
+  },
+  {
+    id: 3,
+    name: "3080 Graphics Card",
+    price: "100$",
+    category: "gpu",
+    image: Gpu3080,
+    preview: Gpu3080Preview,
+  },
+  {
+    id: 4,
+    name: "3090 Graphics Card",
+    price: "100$",
+    category: "gpu",
+    image: Gpu3090,
+    preview: Gpu3090Preview,
+  },
+  {
+    id: 5,
+    name: "Ducky Mini",
+    price: "100$",
+    category: "keyboard",
+    image: duckyMini,
+    preview: duckyMiniPreview,
+  },
+  {
+    id: 6,
+    name: "Keychron K2",
+    price: "100$",
+    category: "keyboard",
+    image: keychronK2,
+    preview: keychronK2Preview,
+  },
+  {
+    id: 7,
+    name: "Keychron K6",
+    price: "100$",
+    category: "keyboard",
+    image: keychronK6,
+    preview: keychronK6Preview,
+  },
+  {
+    id: 8,
+    name: "Keychron K8",
+    price: "100$",
+    category: "keyboard",
+    image: keychronK8,
+    preview: keychronK8Preview,
+  },
+];
+
+export { products };
