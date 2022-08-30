2a278a6f7030bb3a74f37b88ebef931ff63c0b0e
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-08-30 20:23:35 +0530

    router deployment fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Header/Navbar";
 import { HomePage } from "./components/Home/HomePage";
